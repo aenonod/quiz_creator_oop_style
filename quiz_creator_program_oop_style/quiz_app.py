ba007227fbce01a87eb8e00b949f470b39391945
aenonod <abegail.nonod13@gmail.com>
2025-05-28 21:47:40 +0800

    docs: add pseudocode to quiz_app.py

---
+++ quiz_creator_program_oop_style/quiz_app.py
@@ -0,0 +1,11 @@
+# quiz_app.py -> to get the filename and questions also serves as the main menu
+# import the Question class
+# import the QuizFileManager class
+
+# under class QuizApp:
+# def for constructor to initialize new object
+# def to get the filename
+# def to get the data (question, choices, and correct answer) from the user
+# def to create/edit a quiz
+# def to view a quiz
+# def to run the program (main menu)
