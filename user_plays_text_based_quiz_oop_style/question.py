56ee56f56c8250ce30c26b1df1157e42e90d40f7
aenonod <abegail.nonod13@gmail.com>
2025-05-29 03:16:00 +0800

    feat: update the code and the storing/handling of data

--- user_plays_text_based_quiz_oop_style/question.py
+++ user_plays_text_based_quiz_oop_style/question.py
@@ -2,26 +2,23 @@
 
 class Question:
     # constructor to initialize new object
-    def __init__(self, item, choices, correct_ans_text):
+    def __init__(self, item, choices, correct_ans_letter):
         self.item = item
         self.choices = choices
-        self.correct_ans_text = correct_ans_text
-        self.correct_ans_letter = self.get_correct_ans_letter()
-    
-    # determine the letter (a, b, c, d) for the correct answer
-    def get_correct_ans_letter(self):
-        choice_letters = ['a', 'b', 'c', 'd']
-        for i, choice in enumerate(self.choices):
-            if choice.lower() == self.correct_ans_text.lower():
-                return choice_letters[i]
-        return ""
+        self.correct_ans_letter = correct_ans_letter.lower()
+
+        choice_map = {'a': 0, 'b': 1, 'c': 2, 'd': 3}
+        if self.correct_ans_letter in choice_map and len(choices) > choice_map[self.correct_ans_letter]:
+            self.correct_ans_letter = choices[choice_map[self.correct_ans_letter]]
+        else:
+            self.correct_ans_text = "N/A"
         
     # format the question data for writing to a text file
     def file_format(self):
         output = f"\nQuestion: {self.item}\n"
         for i, choice in enumerate(self.choices):
             output += f"{chr(97 + i)}) {choice}\n" # chr(97) = 'a'
-        output += f"Correct answer: {self.correct_ans_letter}) {self.correct_ans_text}\n"
+        output += f"Correct answer: {self.correct_ans_letter}\n"
         return output
         
     # easy printing of a question object
