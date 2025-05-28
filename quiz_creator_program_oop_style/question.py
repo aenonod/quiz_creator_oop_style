fb83f4cd24df1c72f0028e7c571186a18985023a
aenonod <abegail.nonod13@gmail.com>
2025-05-28 20:12:26 +0800

    docs: add pseudocode to question.py

---
+++ quiz_creator_program_oop_style/question.py
@@ -0,0 +1,7 @@
+# question.py -> to format or organize provided questions by the user
+
+# under class Question:
+# def for constructor to initialize new object
+# def to determine the letter (a, b, c, d) for the correct answer
+# def to format the question data for writing to a text file
+# def for easy printing of a question object
