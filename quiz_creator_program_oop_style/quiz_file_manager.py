6e7395b6e600a3a8b392c1675a8c908c8db1f01a
aenonod <abegail.nonod13@gmail.com>
2025-05-28 21:03:33 +0800

    docs: add pseudocode to quiz_file_manager.py

---
+++ quiz_creator_program_oop_style/quiz_file_manager.py
@@ -0,0 +1,7 @@
+# quiz_file_manager.py -> to handle file naming as well as saving and loading questions
+
+# under class QuizFileManager:
+# def for constructor to initialize new object
+# def to ensure filename should have txt extension
+# def to save question
+# def to load/read the quiz
