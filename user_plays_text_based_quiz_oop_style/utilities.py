43207266ad70512761686bd1715bf8230733754b
aenonod <abegail.nonod13@gmail.com>
2025-05-29 12:26:39 +0800

    refactor: add @staticmethod to call the ff defs

--- user_plays_text_based_quiz_oop_style/utilities.py
+++ user_plays_text_based_quiz_oop_style/utilities.py
@@ -7,14 +7,17 @@ import time
 
 class Utilities:
     # def to clear screen
+    @staticmethod
     def clear_screen():
         os.system('cls' if os.name == 'nt' else 'clear')
         
     # def for time sleep
+    @staticmethod
     def pause(seconds):
         time.sleep(seconds)
         
     # def for display message with time of delay
+    @staticmethod
     def display_message(message, delay=1.5):
         print(message)
         Utilities.pause(delay)
