a368326fe97ded1b8fc3d4f21a721e842354ee9c
aenonod <abegail.nonod13@gmail.com>
2025-05-29 12:23:51 +0800

    feat: add program code to clear screen, time sleep, and display msg

--- user_plays_text_based_quiz_oop_style/utilities.py
+++ user_plays_text_based_quiz_oop_style/utilities.py
@@ -1,8 +1,20 @@
 # utilities.py -> for clear screen, time sleep, and display message
-# import os module
-# import time module
 
-# under class Utilities:
-# def to clear screen
-# def for time sleep
-# def for display message with time of delay
+# import os module
+import os
+# import time module
+import time
+
+class Utilities:
+    # def to clear screen
+    def clear_screen():
+        os.system('cls' if os.name == 'nt' else 'clear')
+        
+    # def for time sleep
+    def pause(seconds):
+        time.sleep(seconds)
+        
+    # def for display message with time of delay
+    def display_message(message, delay=1.5):
+        print(message)
+        Utilities.pause(delay)
