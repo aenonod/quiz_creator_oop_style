c75eae510171ce73196dc60bc6f97f1627719bc9
aenonod <abegail.nonod13@gmail.com>
2025-05-29 14:27:27 +0800

    add program code to run quizmate

---
+++ user_plays_text_based_quiz_oop_style/main.py
@@ -0,0 +1,9 @@
+# main.py -> to run the whole program
+
+# import QuizApp class
+from quiz_app import QuizApp
+
+# call out QuizApp class to run the whole code
+if __name__ == "__main__":
+    app = QuizApp()
+    app.run()
