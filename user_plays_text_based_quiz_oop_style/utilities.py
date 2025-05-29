949861e0984ff43ab927d737534e524902a2b51a
aenonod <abegail.nonod13@gmail.com>
2025-05-29 12:07:30 +0800

    docs: add pseudocode to utilities.py

---
+++ user_plays_text_based_quiz_oop_style/utilities.py
@@ -0,0 +1,8 @@
+# utilities.py -> for clear screen, time sleep, and display message
+# import os module
+# import time module
+
+# under class Utilities:
+# def to clear screen
+# def for time sleep
+# def for display message with time of delay
